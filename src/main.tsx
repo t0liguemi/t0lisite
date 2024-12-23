6036730e213a9f1fbb9d88697a441571a05052c2
Miguel <toliguemi94@gmail.com>
2024-12-23 03:53:15 -0300

    build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Navbar from "./ui/components/navbar.tsx";
 import Music from "./views/music.tsx";
 import CoffeeMain from "./views/coffeeMain.tsx";
 import Home from "./views/home.tsx";
